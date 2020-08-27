f0ceb5138de3b34da903e4083a4f4cfc0af7aae7
Scott Bickell <bickellworks@gmail.com>
2020-08-27 11:42:40 -0600

    Add final versions

---
+++ output to sitefinity/scratch.js
@@ -0,0 +1,5 @@
+$('#modal-ar, #modal-aw, #modal-long-a, #modal-short-a, #modal-short-e, #modal-long-e, #modal-er, #modal-short-i, #modal-long-o, #modal-long-oo, #modal-short-o, #modal-oo, #modal-or, #modal-ow, #modal-oy, #modal-long-u, #modal-short-u, #modal-b, #modal-c, #modal-d, #modal-f, #modal-g, #modal-h, #modal-j, #modal-k, #modal-l, #modal-m, #modal-n, #modal-ng, #modal-p, #modal-r, #modal-s, #modal-sh, #modal-t, #modal-th, #modal-th-hard, #modal-v, #modal-w, #modal-wh, #modal-x, #modal-y, #modal-z').appendTo('body').modal('show');
+
+/*
+
+*/
